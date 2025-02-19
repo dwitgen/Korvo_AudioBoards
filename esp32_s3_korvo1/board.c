7c1ac40c01c635df6a3e0dd1face80d304e4ac45
dwit <dwitgen+github@gmail.com>
2025-02-19 12:25:28 -0500

    Updates for audio_board_key_init

--- esp32_s3_korvo1/board.c
+++ esp32_s3_korvo1/board.c
@@ -96,15 +96,15 @@ esp_err_t audio_board_key_init(esp_periph_set_handle_t set)
             .atten    = ADC_ATTEN_DB_12,
             .bitwidth = ADC_BITWIDTH_12,
         },
-        .arr = &adc_btn_tag,  // your adc_arr_t instance(s)
-        .arr_size = 1,
+        //.arr = &adc_btn_tag,  // your adc_arr_t instance(s)
+        //.arr_size = 1,
     };
     adc_arr_t adc_btn_tag = ADC_DEFAULT_ARR();
     int btn_array[7] = {190, 600, 1000, 1375, 1775, 2195, 3000};
     adc_btn_tag.adc_ch = INPUT_BUTOP_ID;
     adc_btn_tag.adc_level_step = btn_array;
-    //adc_btn_cfg.arr = &adc_btn_tag;
-    //adc_btn_cfg.arr_size = 1;
+    adc_btn_cfg.arr = &adc_btn_tag;
+    /adc_btn_cfg.arr_size = 1;
     esp_periph_handle_t adc_btn_handle = periph_adc_button_init(&adc_btn_cfg);
     AUDIO_NULL_CHECK(TAG, adc_btn_handle, return ESP_ERR_ADF_MEMORY_LACK);
     return esp_periph_start(set, adc_btn_handle);
